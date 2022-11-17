85867eafdf89af50bce45edd427bed003a05d0dd
Marina Ivantsova <marineivantsova@yandex.ru>
2022-11-17 09:51:08 +0300

    2.16. Начинаем программировать

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const PromoFilm = {
+  FilmsName: 'The Grand Budapest Hotel',
+  Genre: 'Drama',
+  ReleaseDate: 2014,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      promoFilm={PromoFilm}
+    />
   </React.StrictMode>,
 );
